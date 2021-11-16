cbcf2d94c74e8bec9407f204ac313f9c1bc9a7c2
tscopy <94494517+tscopy@users.noreply.github.com>
2021-11-16 18:18:57 -0500

    Create tscopy.js

---
+++ tscopy.js
@@ -0,0 +1,18 @@
+// Merci de respecter mon travail et ne pas copier ce script.
+// Script créé par Timothé Sévigny
+//timsev6.github.io
+//tscopy.gitub.io/tscopy.js
+
+
+
+function prot() {
+  var imgs = document.getElementsByTagName("img");
+  for(var i=0; i<imgs.length; i++)
+  imgs[i].oncontextmenu = msg;
+}
+
+
+
+function msg() {
+  window.alert("Se site est proteger par tscopy.github.io\n\nLa copie d'image est interdite sur ce site.");return false;
+}
